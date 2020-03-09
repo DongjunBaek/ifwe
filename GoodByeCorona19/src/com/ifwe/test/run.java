2185a003a8ad99692daeea06c324623fcc887002
shinhyungchul <vkoreav11@naver.com>
2020-03-09 13:07:48 +0900

    200309 코로나꺼져~

--- GoodByeCorona19/src/com/ifwe/test/run.java
+++ GoodByeCorona19/src/com/ifwe/test/run.java
@@ -4,6 +4,9 @@ public class run {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to GoodByeConona19 project");
+		System.out.println("어쩌라고");
+		System.out.println("흥흥");
+		System.out.println("코로나19꺼져~");
 	}
 
 }
