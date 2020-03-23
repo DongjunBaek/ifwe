1166f5682cfc3bb968bc54692a98ea9a1c0a5bcc
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-03-24 02:49:59 +0900

    add function 게시글 조회 목록 출력하기
    0324 동준 게시글 조회목록 구현완료
    
    중요한 점
    메인화면 게시판 이동을 보드관련 패키지로 변경하여 진행함.
    이유 : board service와 dao를 사용하기위해서이다.

--- Ifwe.0.3/src/main/java/com/kh/ifwe/board/model/dao/BoardDAO.java
+++ Ifwe.0.3/src/main/java/com/kh/ifwe/board/model/dao/BoardDAO.java
@@ -1,9 +1,13 @@
 package com.kh.ifwe.board.model.dao;
 
+import java.util.List;
+
 import com.kh.ifwe.board.model.vo.Board;
 
 public interface BoardDAO {
 
 	int insertBoard(Board board);
+
+	List<Board> selectOne(String boardCategory);
 
 }
