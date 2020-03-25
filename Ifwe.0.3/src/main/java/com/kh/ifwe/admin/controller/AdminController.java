f5ba4e21fba3fc60eb8d14152495189b02545a7f
jangyeoju <59955338+jangyeoju@users.noreply.github.com>
2020-03-25 16:57:57 +0900

    admin member 수정

--- Ifwe.0.3/src/main/java/com/kh/ifwe/admin/controller/AdminController.java
+++ Ifwe.0.3/src/main/java/com/kh/ifwe/admin/controller/AdminController.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -44,60 +43,32 @@ public class AdminController {
 	}
 	
 	@GetMapping("/member.do")
-	public ModelAndView member(@RequestParam(value="cPage",defaultValue="1")int cPage) {
+	public String member() {
+		return "/admin/adminMember";
+	}
+	
+	/**
+	 * 
+	 * @param cPage
+	 * @param memberRole
+	 * @return
+	 */
+	@GetMapping("/memberList.do")
+	@ResponseBody
+	public List<Member> memberList(@RequestParam(value="cPage",defaultValue="1")int cPage, @RequestParam(value="memberRole", required=false, defaultValue="member")String memberRole) {
 		log.debug("회원목록 페이지");
 		
-		ModelAndView mav = new ModelAndView();
-		
 		final int numPerPage = 10;
 		
 		//업무로직 처리
-		List<Member> list = adminService.selectMemberList(cPage, numPerPage);
+		List<Member> list = adminService.selectMemberList(cPage, numPerPage, memberRole);		
+		
 		log.debug("list="+list);
 		
-		mav.addObject("list",list);
-		mav.setViewName("admin/adminMember");
-		
-		return mav;
+		return list;
 	}
 	
-	@GetMapping("/dropMember.do")
-	public ModelAndView dropMember(@RequestParam(value="cPage",defaultValue="1")int cPage) {
-		
-		log.debug("탈퇴회원 페이지");
-	
-		ModelAndView mav = new ModelAndView();
-		
-		final int numPerPage = 10;
-		
-		//업무로직 처리
-		List<Member> dropList = adminService.selectDropMemberList(cPage, numPerPage);
-		log.debug("dropList{}=",dropList);
-		
-		mav.addObject("dropList",dropList);
-		mav.setViewName("admin/adminMember");
-		
-		return mav;
-	}
-	
-	@GetMapping("/dormantMember.do")
-	public ModelAndView dormantMember(@RequestParam(value="cPage", defaultValue="1")int cPage) {
-		log.debug("휴면회원 페이지");
-	
-		ModelAndView mav = new ModelAndView();
-		
-		final int numPerPage = 10;
-		
-		//업무로직 처리
-		List<Member> dormantList = adminService.selectDormantMemberList(cPage, numPerPage);
-		log.debug("dormantList{}=",dormantList);
-		
-		mav.addObject("dormantList",dormantList);
-		mav.setViewName("admin/adminMember");
-			
-		return mav;
-	}
-	
+
 	@GetMapping("/cancel.do")
 	public String cancel(@RequestParam("memberCode") int memberCode, RedirectAttributes redirectAttributes) {
 		
@@ -109,6 +80,29 @@ public class AdminController {
 		redirectAttributes.addFlashAttribute("msg", result>0?"변경성공":"변경실패");
 		
 		return "/admin/adminMember";
+	}
+	
+	@GetMapping("/drop.do")
+	public String drop(@RequestParam("memberCode") int memberCode, RedirectAttributes redirectAttributes) {
+		
+		Member member = adminService.selectOne(memberCode);
+		log.debug("member{}=",member);
+		
+		int result = adminService.dropMember(member,memberCode);
+		
+		redirectAttributes.addFlashAttribute("msg", result>0?"변경성공":"변경실패");
+		
+		return "/admin/adminMember";
+	}
+	
+	@GetMapping("/searchMember.do")	
+	@ResponseBody
+	public List<Member> search(@RequestParam(value="memberName", required=false) String memberName) {
+		
+		List<Member> searchList = adminService.selectOne(memberName);
+		log.debug("searhList{}=",searchList);
+		
+		return searchList;
 	}
 	
 	
