7d3fc302ff06fb7187e583a28e57c3fc2d278875
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-04-03 01:41:16 +0900

    update crawling function
    0403 crawling
    
    크롤링 정보 가져오기 자료 준비 완료
    호출 메소드 정리

--- Ifwe.0.3/src/main/java/com/kh/ifwe/common/crawling/controller/myCrawlingController.java
+++ Ifwe.0.3/src/main/java/com/kh/ifwe/common/crawling/controller/myCrawlingController.java
@@ -21,6 +21,8 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kh.ifwe.common.crawling.model.myCrawling;
@@ -29,10 +31,11 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@Component
+@Controller
 @Getter
 @Setter
 @Log4j
+@RequestMapping("/myCrawling")
 public class myCrawlingController {
 	
 	private Map<Integer,myCrawling > mapSong;
@@ -44,7 +47,31 @@ public class myCrawlingController {
 	@Autowired 
 	private ServletContext servletContext;
 	
-
+	@ResponseBody
+	@GetMapping("/song.do")
+	public Map<Integer, myCrawling> getMapSong(){
+		return mapSong;
+	}
+	@ResponseBody
+	@GetMapping("/musical.do")
+	public Map<Integer, myCrawling> getMapMusical(){
+		return mapMusical;
+	}
+	@ResponseBody
+	@GetMapping("/concert.do")
+	public Map<Integer, myCrawling> getMapConcert(){
+		return mapConcert;
+	}
+	@ResponseBody
+	@GetMapping("/theater.do")
+	public Map<Integer, myCrawling> getMapTheater(){
+		return mapTheater;
+	}
+	@ResponseBody
+	@GetMapping("/study.do")
+	public Map<Integer, myCrawling> getMapStudy(){
+		return mapStudy;
+	}
 	
 //	@PostConstruct
 	public Map<Integer,myCrawling > mapSong() {
@@ -58,12 +85,12 @@ public class myCrawlingController {
 			Elements elements = doc.select("span.ellipsis");
 			int idx = 0;
 			int codeForIdx = 0;
-			// 3. view 
 			
+			
+			// 3. view 
 			mapSong = new HashMap<Integer, myCrawling>();
 			myCrawling myCrawling =null;
 			for (Element element : elements) {
-//				System.out.println(++idx+" idx");
 //				System.out.println(element);
 				if(idx<21) {
 					++idx;
@@ -74,17 +101,35 @@ public class myCrawlingController {
 					}
 					else {
 						myCrawling.setContentsInfo(element.text());
-	//					/* System.out.println(r); */
 						
-						mapSong.put(idx, myCrawling);
-						System.out.println(idx);
+						mapSong.put(idx/2, myCrawling);
 					}
 				}
 
 			}
+			
+			Elements elementsHref = doc.select("td.name a");
+			idx = 1;
+			codeForIdx = 1;
+			
+			for (Element element : elementsHref) {
+					
+						if(idx == (codeForIdx * 4 - 3) ) {
+							System.out.println("https://music.naver.com"+element.attr("href"));
+							
+							mapSong.get(codeForIdx).setContentsHref("https://music.naver.com"+element.attr("href"));
+							
+							codeForIdx++;
+						}
+					idx++;
+					if(codeForIdx > 10) {
+						break;
+					}
+			}
+
 			imgCrawlerForSong(crawlingURL);
 			
-			
+
 		} catch (IOException e) {
 			System.out.println("IOException");
 		}
@@ -122,7 +167,7 @@ public class myCrawlingController {
 		}
 	}
 	
-//	@PostConstruct
+	@PostConstruct
 	public void mapPlays() {
 		mapMusical = new HashMap<Integer, myCrawling>();
 		mapConcert = new HashMap<Integer, myCrawling>();
@@ -146,7 +191,6 @@ public class myCrawlingController {
 				myCrawling.setContentsHref(element.attr("href"));
 				idx++;
 				if(idx>12 && idx<93) {
-//					imgCrawler(myCrawling.getContentsHref());
 										
 					String crawlingURL2 = element.attr("href");					
 					Document doc2= Jsoup.connect(crawlingURL2).get();
@@ -157,16 +201,19 @@ public class myCrawlingController {
 					if(idx>12 && idx<23) {
 						//뮤지컬
 						System.out.println("Crawling Veiw ="+myCrawling.getContentsName());
+						myCrawling.setContentsCode(idx);
 						mapMusical.put(idx, myCrawling);
 						imgCrawlerForPlays(myCrawling.getContentsHref(), idx);
 					}else if(idx>32 && idx<43) {
 						//콘서트
 						System.out.println("Crawling Veiw ="+myCrawling.getContentsName());
+						myCrawling.setContentsCode(idx);
 						mapConcert.put(idx, myCrawling);
 						imgCrawlerForPlays(myCrawling.getContentsHref(), idx);
 					}else if(idx>52 && idx < 63) {
 						//연극
 						System.out.println("Crawling Veiw ="+myCrawling.getContentsName());
+						myCrawling.setContentsCode(idx);
 						mapTheater.put(idx, myCrawling);
 						imgCrawlerForPlays(myCrawling.getContentsHref(), idx);
 					}else if(idx>63)
@@ -225,15 +272,17 @@ public class myCrawlingController {
 			
 			// 3. view 
 			mapStudy = new HashMap<Integer, myCrawling>();
-//			myCrawling myCrawling = new myCrawling();
 			int idx = 0;
+			int numIdx = 1;
 			System.out.println("http://www.yes24.com/");
 			for (Element element : elements) {
+				myCrawling myCrawling = new myCrawling();
 				++idx;
 				if(idx%2 == 1) {
-					System.out.print("idx = "+ idx);
-					System.out.print(element.text());
-					System.out.println(element.attr("href"));
+					myCrawling.setContentsCode(numIdx);
+					myCrawling.setContentsName(element.text());
+					myCrawling.setContentsHref("http://www.yes24.com"+element.attr("href"));
+					mapStudy.put(numIdx++, myCrawling);
 				}
 				if(idx == 19)
 					break;
@@ -255,7 +304,7 @@ public class myCrawlingController {
 			doc = Jsoup.connect(urls).get();
 			String folder = "books";
 			Elements elements = doc.select(".imgBdr a img");			
-			int page = 0;
+			int page = 1;
 			for (Element e : elements) {
 				String url = e.getElementsByAttribute("src").attr("src");
 				
@@ -264,6 +313,8 @@ public class myCrawlingController {
 				File file = new File(servletContext.getRealPath("/resources/upload/") + folder + "\\" + page + ".jpg");
 				ImageIO.write(jpg, "jpg", file);
 				page += 1;
+				if(page >10)
+					break;
 				
 			}
 		} catch (IOException e1) {
